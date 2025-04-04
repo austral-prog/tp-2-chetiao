6d294f09c48b2d8a7ec4b742c0180c50aef5d957
Tiago Farach <tnfarach@mail.austral.edu.ar>
2025-04-04 07:37:41 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,22 @@
 def change():
     expense = 23.75
     money = 100
+
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print()
+    print("Vuelto")
+
+    print()
+
+    print("Pesos:")
+    resultado = money - expense
+    Decimal = str(resultado)[3 : 5]
+
+    print(int(resultado))
+    print("Centavos")
+    print(Decimal) 
+
+change()
