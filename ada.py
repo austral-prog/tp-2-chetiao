45f8b5032ba9c5ac216efc836bea3a097614ff20
Tiago Farach <tnfarach@mail.austral.edu.ar>
2025-04-04 07:49:31 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,7 +1,7 @@
 first_name = "AdA"
-last_name = "LoVeLAce"
+last_name = " LoVeLAce"
 
 print(first_name.lower().strip()+last_name.lower())
 print(first_name.title().strip()+last_name.title())
 print(first_name.upper().strip()+last_name.upper())
-print(f"{first_name.lower()} {last_name.capitalize()}")
+print(f"    {first_name.lower() + last_name.lower()}")
