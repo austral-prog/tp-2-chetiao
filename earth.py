2ad3c114674ccb59c2fb1dae2b48a908a7bdba3c
Tiago Farach <tnfarach@mail.austral.edu.ar>
2025-04-04 07:59:55 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -9,5 +9,5 @@ x = "Bangladesh"
 
 resultado1, resultado2 = comparar_paises(y, x)
 
-print(f"The result of {y} comes first in the dicctionary than {x} is {resultado1}.")
-print(f"The result of {x} comes first in the dicctionary than {y} is {resultado2}.")
+print(f"The result of {y} comes first in the dictionary than {x} is {resultado1}.")
+print(f"The result of {x} comes first in the dictionary than {y} is {resultado2}.")
