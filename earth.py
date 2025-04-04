d49a4d3d4d9a2f8c93467c67508201ce0953bf0f
Tiago Farach <tnfarach@mail.austral.edu.ar>
2025-04-04 06:59:21 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,13 @@
-def earth():
-    x = "Bangladesh"
-    y = "Barbados"
+def comparar_paises(y, x):
+  if y < x:
+    return True, False
+  else:
+    return False, True
+
+y = "Barbados"
+x = "Bangladesh"
+
+resultado1, resultado2 = comparar_paises(y, x)
+
+print(f"El resultado de {y} viene primero en el diccionario que {x} es {resultado1}.")
+print(f"El resultado de {x} viene primero en el diccionario que {y} es {resultado2}.")
